93fc98001040ad9985bcd33b862edc315377d1a1
Shahna <shahna.c.t666@gmail.com>
2018-03-29 12:04:27 +0530

    first commit

---
+++ CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  gittest
+//
+//  Created by Coding Hands on 29/03/18.
+//  Copyright © 2018 Coding Hands. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
